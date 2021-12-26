1247f036ce4a0f047a4eda6306a1c03af74780a8
MadsenAMS <andre-madsen@hotmail.com>
2021-12-26 09:34:48 -0300

    Changed the output format for the index.js file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const ArrayManager = require ('./assets/js/arraymanager');
 const arrayManager = new ArrayManager();
-const initialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const initialArray = [1, 5, 17, 23, 22, 17, 08];
 
 const mappedArray = arrayManager.MapAndDoubleArray(initialArray);
 const filteredArray = arrayManager.FilterAndReturnEvenArray(initialArray);
 const reducedArray = arrayManager.ReduceAndReturnArraySum(initialArray);
-
+console.log("\nInitial array is [1, 5, 17, 23, 22, 17, 08].\nMapped array uses 'map' to double each number in array.\nFiltered Array returns only even numbers using 'filter'.\nReduced Array returns sum of numbers in array using 'reduce'.\n\n")
 console.log(`Initial Array = ${initialArray}`);
 console.log(`Mapped Array = ${mappedArray}`);
 console.log(`Filtered Array = ${filteredArray}`);
-console.log(`Reduced Array = ${reducedArray}`);
+console.log(`Reduced Array = ${reducedArray}\n`);
